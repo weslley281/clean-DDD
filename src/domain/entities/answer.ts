9f492f8157c73de137cc36172a4d370346965439
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-10-07 06:29:28 -0400

    a

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,10 +1,13 @@
 import { randomUUID } from 'crypto';
 import { Entity } from '../../core/entities/entity';
+import { UniqueEntityID } from '../../core/entities/unique-entity-id';
 
 interface Props {
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
   content: string;
-  authorId: string;
-  questionId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Answer extends Entity<Props> {
--- src/domain/entities/questions.ts
+++ src/domain/entities/questions.ts
@@ -1,11 +1,16 @@
 import { randomUUID } from 'crypto';
 import { Entity } from '../../core/entities/entity';
+import { UniqueEntityID } from '../../core/entities/unique-entity-id';
+import { Slug } from './value-objects/slug';
 
 interface Props {
+  authorId: UniqueEntityID;
+  bestAnswerId: UniqueEntityID;
   title: string;
   content: string;
-  authorId: string;
-  slug: string;
+  slug: Slug;
+  createAt: Date;
+  updateAt?: Date;
 }
 
 export class Question extends Entity<Props> {}
